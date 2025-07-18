5dcbda6c9bdd75c2d38ff423b1f61608f79c09d5
Grace <114320346+gracerhpotter@users.noreply.github.com>
2025-07-18 15:28:53 -0700

    Add files via upload

---
+++ biomaRT_ensemblID.R
@@ -0,0 +1,24 @@
+
+ensembl_dataset <- openxlsx::read.xlsx("~/Downloads/Raghu_Myeloma_CorrectedSscore/sscoreInputs/RNAseq_LOGFC.xlsx")
+data.frame(table(ensembl_dataset$X5))
+
+ids <- ensembl_dataset %>% filter(is.na(X3))
+data.frame(table(ids$X5))
+
+ids <- ids$X1
+
+mart <- biomaRt::useDataset("hsapiens_gene_ensembl", biomaRt::useMart("ensembl"))
+ensembl <- biomaRt::useEnsembl(biomart = "ensembl", dataset = 'hsapiens_gene_ensembl')
+# biomaRt::listAttributes(mart = ensembl)
+gene_symbol_list <- biomaRt::getBM(filters= "ensembl_gene_id", attributes= c("ensembl_gene_id",
+                                                                         "external_gene_name", 
+                                                                         "uniprot_gn_symbol",
+                                                                         "hgnc_symbol"), values = ids, mart = mart)
+
+data_genes <- merge(ensembl_dataset, gene_symbol_list, by.x = "X1", by.y = "ensembl_gene_id") %>%
+  filter(!(hgnc_symbol == "")) %>%
+  dplyr::select(X1, hgnc_symbol)
+
+
+data <- merge(ensembl_dataset, data_genes, by = "X1", all = TRUE)
+openxlsx::write.xlsx(data, "~/Downloads/Raghu_Myeloma_CorrectedSscore/sscoreInputs/RNAseq_LOGFC_biomaRt.xlsx")
